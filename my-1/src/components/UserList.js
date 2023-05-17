87cb95129f1ee6f003b7aeaf7d566a3ec27c0890
F F <ktrnstm@gmail.com>
2023-05-17 19:04:50 +0300

    Добавление в пользователей в список

---
+++ my-1/src/components/UserList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function UserList() {
+  const [users, setUsers] = React.useState(['John', 'Jane', 'Alice']);
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddUser = () => {
+    if (inputValue.trim() !== '') {
+      setUsers([...users, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+      <input type="text" value={inputValue} onChange={handleInputChange}></input>
+      <button onClick={handleAddUser}>Add Usrrrrers</button>
+    </div>
+  );
+}
